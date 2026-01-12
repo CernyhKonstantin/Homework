7d1cd7cb9bb467f79c8a3557ccbfe14a8b7a300c
Konstantin Cernyh <kostik.cernyh@gmail.com>
2026-01-12 19:41:57 +0100

    Add files via upload

---
+++ Homework/Homework/HW 12.01.2026 Task 1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task1_Play
+{
+    class Play
+    {
+        public string Title { get; set; }
+        public string AuthorFullName { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Play(string title, string author, string genre, int year)
+        {
+            Title = title;
+            AuthorFullName = author;
+            Genre = genre;
+            ReleaseYear = year;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {AuthorFullName}");
+            Console.WriteLine($"Genre: {Genre}");
+            Console.WriteLine($"Year: {ReleaseYear}");
+        }
+
+        // Destructor
+        ~Play()
+        {
+            Console.WriteLine($"Destructor called for play: {Title}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Play play = new Play("Hamlet", "William Shakespeare", "Tragedy", 1603);
+            play.DisplayInfo();
+
+            play = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("End of program.");
+        }
+    }
+}
